7174345f4603f64a28dd9a68103166f97c239459
DHIRAWIDODO <dhirayyuwidodo@gmail.com>
2026-09-06 19:09:39 +0700

    update theme sidebar

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!--begin::Sidebar-->
-<aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+<aside class="app-sidebar bg-body-secondary shadow">    
     <!--begin::Sidebar Brand-->
     <div class="sidebar-brand">
         <a href="{{ route('dashboard') }}" class="brand-link">
