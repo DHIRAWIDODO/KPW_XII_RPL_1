4d96cb229515ad266045268d3b656cb307f79bbe
DHIRAWIDODO <dhirayyuwidodo@gmail.com>
2026-09-07 08:32:26 +0700

    Role Databases

--- database/migrations/2026_09_07_012050_create_roles_table.php
+++ database/migrations/2026_09_07_012050_create_roles_table.php
@@ -8,15 +8,15 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['admin', 'user'])->default('user')->after('email');
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // contoh: admin, user
+            $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
-        });
+        Schema::dropIfExists('roles');
     }
 };
